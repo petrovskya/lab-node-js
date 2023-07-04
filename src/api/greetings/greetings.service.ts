6f77fa1185103829d4f2105f00b489af88018f65
petrovskya <petrovska.ya@icloud.com>
2023-07-04 13:06:05 +0300

    fix: change relative paths to absolute

--- src/api/greetings/greetings.service.ts
+++ src/api/greetings/greetings.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { GREETING_TEXT } from 'config/constants';
 
-// import { GREETING_TEXT } from 'constants';
+import { GREETING_TEXT } from 'config/constants';
 
 @Injectable()
 export class GreetingsService {
   getHello(name: string): string {
     return GREETING_TEXT(name);
-    return `Hello, ${name}!`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-// import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app.module';
 import { PORT } from 'config/constants';
 
-// const bootstrapPath = path.resolve(__dirname, '../tsconfig-paths-bootstrap');
-// import(bootstrapPath);
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
