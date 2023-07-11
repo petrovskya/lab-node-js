90786659e34ff96e8b1ac00382b2257b6966b765
Yana Petrovskaya <petrovska.ya@icloud.com>
2023-07-11 18:18:26 +0300

    feat: update notes dto files

--- src/api/notes/dto/create-note.dto.ts
+++ src/api/notes/dto/create-note.dto.ts
@@ -1,27 +1,22 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsString, Length, MinLength } from 'class-validator';
 
-import { NOTE_VALIDATION_SCHEMA } from 'config/constants';
+import {
+  ERROR_VALIDATION_MESSAGES,
+  NOTE_VALIDATION_SCHEMA,
+} from 'config/constants';
 
 export class CreateNoteDto {
-  @IsString()
-  @IsNotEmpty()
-  _id: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(NOTE_VALIDATION_SCHEMA.TITLE.MIN_LENGTH)
+  @IsString({ message: ERROR_VALIDATION_MESSAGES.NOT_STRING })
+  @MinLength(NOTE_VALIDATION_SCHEMA.TITLE.MIN_LENGTH, {
+    message: ERROR_VALIDATION_MESSAGES.NOTE_TITLE,
+  })
   title: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(NOTE_VALIDATION_SCHEMA.CONTENT.MIN_LENGTH)
-  @MaxLength(NOTE_VALIDATION_SCHEMA.CONTENT.MAX_LENGTH)
+  @IsString({ message: ERROR_VALIDATION_MESSAGES.NOT_STRING })
+  @Length(
+    NOTE_VALIDATION_SCHEMA.CONTENT.MIN_LENGTH,
+    NOTE_VALIDATION_SCHEMA.CONTENT.MAX_LENGTH,
+    { message: ERROR_VALIDATION_MESSAGES.NOTE_CONTENT },
+  )
   content: string;
-
-  @IsString()
-  @IsNotEmpty()
-  createdAt: string;
-
-  @IsString()
-  updatedAt: string;
 }
--- src/api/notes/dto/index.ts
+++ src/api/notes/dto/index.ts
@@ -1,3 +1,2 @@
-export { NoteDto } from './note.dto';
 export { CreateNoteDto } from './create-note.dto';
 export { UpdateNoteDto } from './update-note.dto';
